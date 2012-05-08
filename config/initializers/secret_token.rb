e8d1284140a03a8db0fffd22d334eefcd0a9c1fa
Nick McGuire <pudgeball.mcguire@gmail.com>
2012-05-08 12:12:10 -0400

    Inital commit of VexWebsite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VexWebsite::Application.config.secret_token = 'd62d20ee84ccb7391b8c2ff48eab77a2a4514d5fe1ef80f8bbcd975c8b75b5aabb059d1f8716b1a61742658761e5a9679435970fc57ef16b41f9657abb3da0f6'
