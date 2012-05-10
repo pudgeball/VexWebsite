e57cb9be17d8bd8e9f8a6b91024c3c031c313e97
Nick McGuire <pudgeball.mcguire@gmail.com>
2012-05-10 12:39:51 -0400

    Working on adding validation and has_many updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :name, :region, :spotsAvailable
+  
+  validates :spotsAvailable, :region, :date, :name
+            :presence => true
+            
+  validates :spotsAvailable, 
+            :numericality => { :only_integer => true,
+                                :greater_than_or_equal_to => 0,
+                                :less_than_or_equal_to => 36 }
+  
+  has_many :eventRecords
+  has_many :eventAttendees
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,6 @@
 class School < ActiveRecord::Base
   attr_accessible :name, :region
+  
+  has_many :teams
+  has_many :users
 end
