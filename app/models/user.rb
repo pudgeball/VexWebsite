45d1e3eb2deeac8be8fb831ab630c8990fdb846c
Mikkel Harris <mikkel.harris@gmail.com>
2012-05-14 10:12:15 -0400

    user password added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   belongs_to :school
   
-  attr_accessible :email, :name, :role, :school_id
+  attr_accessible :email, :name, :role, :school_id, :password
   
-  validates :email, :name, :role, :school_id,
+  validates :email, :name, :role, :school_id, :password,
             presence: true
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120513161422) do
+ActiveRecord::Schema.define(:version => 20120514134143) do
 
   create_table "applications", :force => true do |t|
     t.string   "teamName"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(:version => 20120513161422) do
     t.integer  "school_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "password"
   end
 
   add_index "users", ["school_id"], :name => "index_users_on_school_id"
