63e448761784063d17f64e3bbaf286e00d293abe
Nick McGuire <pudgeball.mcguire@gmail.com>
2012-05-17 13:05:13 -0400

    Seeding!
    I added some seeds so now we have wonderful data to play with when
    first cloned :3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # REALLY NEED TO MAKE SOME DATA, HIGH PRIORITY!
+
+applications = Application.create([{schoolName: 'Burlington Central High School', teamName: 'Awesomenauts', userEmail: 'pudgeball@me.com', userName: 'Nick McGuire'}, {schoolName: 'MMR High School', teamName: 'Hammy', userEmail: 'hammeld@hdsb.ca', userName: 'Dave Hammel'}])
+
+schools = School.create([{name: 'Burlington Central High School', region: 'South'}, {name: 'MMR High School', region: 'South'}])
+
+teams = Team.create([{name:'Awesomenauts', numStudents:4, school_id: schools.first.id}])
+
+events = Event.create([{spotsAvailable:20, region: 'South', date:DateTime.new(2012, 6, 14), name:'VEX HDSB'}])
+
+users = User.create([{email: 'pudgeball@me.com', name: 'Nick McGuire', role: 'Admin', school_id: schools.first.id, password:'password'}])
+
+event_records = EventRecord.create([{loss:0, win:1, tie:2, event_id:events.first.id, team_id:teams.first.id}])
+
+event_attendees = EventAttendee.create([{event_id: events.first.id, team_id:teams.first.id}])
